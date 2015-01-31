f6c6d5958d347809a7d25efb4ffbf04fa8251dbc
mallius <mallius@qq.com>
2015-01-31 22:49:18 +0800

    增加注释

--- TCPIPSocketsInC/TCPserver/TCPEchoServer4.c
+++ TCPIPSocketsInC/TCPserver/TCPEchoServer4.c
@@ -14,27 +14,33 @@ int main(int argc, char *argv[])
 	if((servSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
 		DieWithSystemMessage("socket() failed");
 
-	struct sockaddr_in servAddr;
-	memset(&servAddr, 0, sizeof(servAddr));
-	servAddr.sin_family = AF_INET;
-	servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servAddr.sin_port = htons(servPort);
+	/* Construct local address structure */
+	struct sockaddr_in servAddr;			/* Local address */
+	memset(&servAddr, 0, sizeof(servAddr));		/* Zero out Construct */
+	servAddr.sin_family = AF_INET;			/* IPv4 address family */
+	servAddr.sin_addr.s_addr = htonl(INADDR_ANY);	/* Any incoming interface */
+	servAddr.sin_port = htons(servPort);		/* Local Port */
 	
+	/* Bind to the Local address */
 	if(bind(servSock, (struct sockaddr*)&servAddr, sizeof(servAddr)) < 0)
 		DieWithSystemMessage("bind() failed");
 
+	/* Mark the socket so it will listen for incoming connections */
 	if(listen(servSock, MAXPENDING) < 0)
 		DieWithSystemMessage("listen() failed");
 
-	for(;;){
-		struct sockaddr_in clntAddr; 
+	for(;;){					/* Run forever */
+		struct sockaddr_in clntAddr; 		/* Client address */
 		socklen_t clntAddrLen = sizeof(clntAddr);
 
+		/* Wait for a client to connect */
 		int clntSock = accept(servSock, (struct sockaddr*)&clntAddr, &clntAddrLen);
 		if(clntSock < 0)
 		{
 			DieWithSystemMessage("accept() failed");
 		}
+
+		/* clntSock is connected to a client */
 		char clntName[INET_ADDRSTRLEN];
 		if(inet_ntop(AF_INET, &clntAddr.sin_addr.s_addr, clntName, sizeof(clntName)) != NULL)
 			printf("Handling client %s/%d\n", clntName, ntohs(clntAddr.sin_port));

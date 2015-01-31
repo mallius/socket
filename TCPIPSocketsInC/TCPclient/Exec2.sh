89569cdfdb687dd74b03ab878ed5a8f2794acd51
mallius <mallius@qq.com>
2015-01-31 17:32:30 +0800

    add Exec2.sh

---
+++ TCPIPSocketsInC/TCPclient/Exec2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+./TCPEchoClient4 192.168.1.6 "info1" 50
+./TCPEchoClient4 192.168.1.6 "info2" 50
+./TCPEchoClient4 192.168.1.6 "info3" 50
+./TCPEchoClient4 192.168.1.6 "info4" 50
+./TCPEchoClient4 192.168.1.6 "info5" 50
+./TCPEchoClient4 192.168.1.6 "info6" 50

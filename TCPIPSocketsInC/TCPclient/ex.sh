90ea55b903fc532577abc17711a606cbb69a170c
mallius <mallius@qq.com>
2015-01-31 17:21:53 +0800

    ex.sh

---
+++ TCPIPSocketsInC/TCPclient/ex.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+./TCPEchoClient4 192.168.1.6 "info1" 50
+./TCPEchoClient4 192.168.1.6 "info2" 50
+./TCPEchoClient4 192.168.1.6 "info3" 50
+./TCPEchoClient4 192.168.1.6 "info4" 50
+./TCPEchoClient4 192.168.1.6 "info5" 50

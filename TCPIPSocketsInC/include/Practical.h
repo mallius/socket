1450388251d9388cf897e7fc95ab96ac01260b92
mallius <mallius@qq.com>
2015-02-01 22:20:32 +0800

    增加头文件

--- TCPIPSocketsInC/include/Practical.h
+++ TCPIPSocketsInC/include/Practical.h
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
-#include <string.h>
 #include <pthread.h>
 #include <sys/time.h>
 #include <fcntl.h>

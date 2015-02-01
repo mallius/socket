3f663e605462e0419589428f13c7109706f70879
mallius <mallius@qq.com>
2015-02-01 21:50:32 +0800

    修改IPv6

--- TCPIPSocketsInC/TCPserver6/TCPEchoServer6.c
+++ TCPIPSocketsInC/TCPserver6/TCPEchoServer6.c
@@ -11,15 +11,15 @@ int main(int argc, char *argv[])
 	printf("servPort:[%d]\n", servPort);
 
 	int servSock;
-	if((servSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+	if((servSock = socket(AF_INET6, SOCK_STREAM, IPPROTO_TCP)) < 0)
 		DieWithSystemMessage("socket() failed");
 
 	/* Construct local address structure */
-	struct sockaddr_in servAddr;			/* Local address */
+	struct sockaddr_in6 servAddr;			/* Local address */
 	memset(&servAddr, 0, sizeof(servAddr));		/* Zero out Construct */
-	servAddr.sin_family = AF_INET;			/* IPv4 address family */
-	servAddr.sin_addr.s_addr = htonl(INADDR_ANY);	/* Any incoming interface */
-	servAddr.sin_port = htons(servPort);		/* Local Port */
+	servAddr.sin6_family = AF_INET6;			/* IPv4 address family */
+	servAddr.sin6_addr = in6addr_any;		/* Any incoming interface */
+	servAddr.sin6_port = htons(servPort);		/* Local Port */
 	
 	/* Bind to the Local address */
 	if(bind(servSock, (struct sockaddr*)&servAddr, sizeof(servAddr)) < 0)
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
 		DieWithSystemMessage("listen() failed");
 
 	for(;;){					/* Run forever */
-		struct sockaddr_in clntAddr; 		/* Client address */
+		struct sockaddr_in6 clntAddr; 		/* Client address */
 		socklen_t clntAddrLen = sizeof(clntAddr);
 
 		/* Wait for a client to connect */
@@ -41,9 +41,9 @@ int main(int argc, char *argv[])
 		}
 
 		/* clntSock is connected to a client */
-		char clntName[INET_ADDRSTRLEN];
-		if(inet_ntop(AF_INET, &clntAddr.sin_addr.s_addr, clntName, sizeof(clntName)) != NULL)
-			printf("Handling client %s/%d\n", clntName, ntohs(clntAddr.sin_port));
+		char clntName[INET6_ADDRSTRLEN];
+		if(inet_ntop(AF_INET6, &clntAddr.sin6_addr.s6_addr, clntName, sizeof(clntName)) != NULL)
+			printf("Handling client %s/%d\n", clntName, ntohs(clntAddr.sin6_port));
 		else
 			puts("Unable to get client address");
 		HandleTCPClient(clntSock);

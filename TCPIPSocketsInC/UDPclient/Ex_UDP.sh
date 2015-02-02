32f53ffe31393ce58859552fd32483d217cd2e18
mallius <mallius@qq.com>
2015-02-02 20:01:57 +0800

    add Ex_UDP.sh

---
+++ TCPIPSocketsInC/UDPclient/Ex_UDP.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+./UDPEchoClient 192.168.1.5 "udp Echo1" 50
+./UDPEchoClient 192.168.1.5 "udp Echo2" 50
+./UDPEchoClient 192.168.1.5 "udp Echo3" 50
+./UDPEchoClient 192.168.1.5 "udp Echo4" 50
+./UDPEchoClient 192.168.1.5 "udp Echo5" 50
+./UDPEchoClient 192.168.1.5 "udp Echo6" 50
